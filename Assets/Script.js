e12c782ce2c9a7d197f677c2dcbf277eeb364251
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-20 10:01:59 -0700

    First page of website is fully functional.  Safe revert point.

--- Assets/Script.js
+++ Assets/Script.js
@@ -1,9 +1,9 @@
-let $quizSection = document.querySelector("section")
+let $quizSection = document.querySelector(".subSection")
 let $beginQuiz = document.getElementById("quizBeginButton")
 let $timer = document.getElementById("timeClock")
 let index = 0;
 let $runningTime = 0;
-
+// array of questions below.
 const questions = [
     { question: "Place holder question",
     correct: "correct answer",
@@ -11,25 +11,37 @@ const questions = [
     
     { question: "Another question", 
     correct: "right answer",
-    wrong: "Incorrect answer"}];
-    
+    wrong: "Incorrect answer"}
+];
+//Function that add times to the clock when question is missed.
 function tryAgain(){
         $runningTime = $runningTime + 5;
         $timer.textContent = $runningTime;
 };
-
+//function that starts the time.
 function startTime(){
         timeInterval = setInterval(function(){
             $runningTime++;
             $timer.textContent = $runningTime;
         }, 1000);
 };
-    
+//Creates the end quiz screen that allows you too enter your initials.
+function createEndQuiz(){
+    let $h1 = document.createElement("h1");
+    let $p = document.createElement("p");
+    let $input = document.createElement("input");
+    $h1.textContent= (`Congratulations, you finished!`);
+    $p.textContent= (`You finished with a score of ${$runningTime}, type your initials to save your score.`);
+    $quizSection.appendChild($h1);
+    $quizSection.appendChild($p);
+    document.querySelector("form").appendChild($input);
+};
+//function that creates the question and answers and appends them.
 function renderQuestions(){
-    $quizSection.innerHTML = ""; //clears display
-    let $h1 = document.createElement("h1"); //creates h1 tags
-    $h1.textContent = `${questions[index].question}`; //adds content to h1 tags
-    $quizSection.appendChild($h1);  //makes h1 tags visible
+    $quizSection.innerHTML = ""; 
+    let $h1 = document.createElement("h1"); 
+    $h1.textContent = `${questions[index].question}`; 
+    $quizSection.appendChild($h1);  
     let $buttonTrue = document.createElement("button");
     let $buttonFalse = document.createElement("button");
     $buttonTrue.textContent = `${questions[index].correct}`;
@@ -37,38 +49,28 @@ function renderQuestions(){
     $quizSection.appendChild($buttonTrue);
     $quizSection.appendChild($buttonFalse);
     $buttonTrue.addEventListener("click", nextQuestion);
-    $buttonFalse.addEventListener("click", tryAgain)
+    $buttonFalse.addEventListener("click", tryAgain);
 };
-
-function nextQuestion(e){
+//function that checks if out of questions and makes highScore screen then saves data.
+function nextQuestion(){
     if (index < questions.length - 1){
         index++;
         renderQuestions();
     }else{
         clearInterval(timeInterval);
-        e.preventDefault()
         $quizSection.innerHTML ="";
-        let $h1 = document.createElement("h1");
-        let $p = document.createElement("p");
-        let $input = document.createElement("input");
-        $h1.textContent= (`Congratulations, you finished!`);
-        $p.textContent= (`You finished with a score of ${timeInterval}, type your initials below to save your score.`);
-        $quizSection.appendChild($h1)
-        $quizSection.appendChild($p)
-        $quizSection.appendChild($input)
-        initials = $input.value;
-        localStorage.setItem("initials", initials)
+        createEndQuiz();
     };
 };
-
-    
-    
-
-
-
-$beginQuiz.addEventListener("click", function(event){
-    event.stopImmediatePropagation();
+//Button that starts all the functions above.
+$beginQuiz.addEventListener("click", function(){
     renderQuestions();
     startTime();
 });
-
+//Saves data when enter is clicked.
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    let $input =document.querySelector("form")[0];
+    const highScore = {score: $runningTime, initials: $input.value};
+    localStorage.setItem("highScoreInfo", JSON.stringify(highScore)); 
+});
